a76a3f56fc5747463954cbdb0247c7d08aadd74e
JinRich <775868259@qq.com>
2019-06-22 08:21:18 +0800

    123

---
+++ js.js
@@ -0,0 +1,84 @@
+window.onload=function () {
+    let main = document.getElementsByClassName('main');
+    console.log(main[1]);
+    console.log(document.getElementById('main'));
+    let char = echarts.init(document.getElementById('main'));
+    let char2 = echarts.init(main[1]);
+    let char3 = echarts.init(main[2]);
+    let option = {
+        title: {
+            text: 'ECharts 入门示例'
+        },
+        tooltip: {},
+        legend: {
+            data:['销量']
+        },
+        xAxis: {
+            data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+        },
+        yAxis: {},
+        series: [{
+            name: '销量',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20]
+        }]
+    };
+    let option2= {
+        xAxis: {
+            type: 'category',
+            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+            type: 'value'
+        },
+        series: [{
+            data: [820, 932, 901, 934, 1290, 1330, 1320],
+            type: 'line'
+        }]
+    };
+    let option3 = {
+        tooltip: {
+            trigger: 'item',
+            formatter: "{a} <br/>{b}: {c} ({d}%)"
+        },
+        legend: {
+            orient: 'vertical',
+            x: 'left',
+            data:['男','女']
+        },
+        series: [
+            {
+                name:'访问来源',
+                type:'pie',
+                radius: ['50%', '70%'],
+                avoidLabelOverlap: false,
+                label: {
+                    normal: {
+                        show: false,
+                        position: 'center'
+                    },
+                    emphasis: {
+                        show: true,
+                        textStyle: {
+                            fontSize: '30',
+                            fontWeight: 'bold'
+                        }
+                    }
+                },
+                labelLine: {
+                    normal: {
+                        show: true
+                    }
+                },
+                data:[
+                    {value:30, name:'男'},
+                    {value:26, name:'女'},
+                ]
+            }
+        ]
+    };
+
+    char.setOption(option);
+    char2.setOption(option2);
+    char3.setOption(option3);
+};
